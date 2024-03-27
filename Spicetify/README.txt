53618b45f1af2396d9ac835a11d8d182cc9517e5
vigneron <163116378+Thanh-vrn1@users.noreply.github.com>
2024-03-27 10:15:04 +0100

    Add files via upload

---
+++ Spicetify/README.txt
@@ -0,0 +1,2 @@
+Pour installer spicetify
+faites clique droit sur le fichier powershell et cliquez sur executer avec powershell
---
+++ Spicetify/Spicetify setup.ps1
@@ -0,0 +1,2 @@
+﻿iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
